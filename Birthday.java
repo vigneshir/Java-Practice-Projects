d6648064606601901e3c73b9a3317e9966b8f32b
vigneshir <108547301+vigneshir@users.noreply.github.com>
2023-03-12 16:18:51 +0530

    Update Birthday.java

--- Birthday.java
+++ Birthday.java
@@ -1,3 +1,16 @@
+/*
+Write a program Birthday.java that takes two integer command-line arguments n and trials and performs the following experiment, trials times:
+
+Choose a birthday for the next person, uniformly at random between 0 and n−1.
+Have that person enter the room.
+If that person shares a birthday with someone else in the room, stop; otherwise repeat.
+
+In each experiment, count the number of people that enter the room.
+Print a table that summarizes the results (the count i, the number of times that exactly i people enter the room, and the fraction of times that i or
+fewer people enter the room) for each possible value of i from 1 until the fraction reaches (or exceeds) 50%.
+*/
+
+
 public class Birthday {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]); // no. of days in sample space
